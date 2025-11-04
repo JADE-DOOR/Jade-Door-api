a78279a1cfe3ac1d0d4c5cf77dbd42be2f71a5dc
Zihao Zhao <zhao.3628@osu.edu>
2025-11-04 14:48:30 -0500

    Page18

---
+++ JADE-DOOR.Data/StoreContext.cs
@@ -0,0 +1,16 @@
+﻿using JADE_DOOR.Domain.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+namespace JADE_DOOR.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
+
